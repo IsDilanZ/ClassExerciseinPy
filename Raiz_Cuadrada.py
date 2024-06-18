49a005faed22d7e13f9808cf8f35ebe05a9a7a29
Andrew <chimboandrew28@gmail.com>
2024-06-18 09:50:53 -0500

    1st commit

---
+++ Raiz_Cuadrada.py
@@ -0,0 +1,8 @@
+
+def SquareRoot_calculator(SquareRootNumber):
+    if SquareRootNumber > -1:
+        print("SquareRoot of the entered Number is ", SquareRootNumber**0.5)
+    else:
+        print("Your value is negative so it is not possible to calculate in the domain")    
+    
+SquareRoot_calculator(-48)  
